6bd65d25f6eb105f4843caa7898a1380100429e7
marshandabdl <marshandaabdul106@student.unsrat.ac.id>
2026-04-26 18:26:45 +0800

    add temp fix route

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,3 @@ Route::get('/', function () {
 
 Route::get('/form', [SpkController::class,'form']);
 Route::post('/hitung',[SpkController::class,'hitung']);
-
-Route::get('/fix-data', function() {
-    \App\Models\NilaiAlternatif::truncate();
-    \App\Models\SubKriteria::truncate();
-    \App\Models\Kriteria::truncate();
-    \App\Models\Alternatif::truncate();
-    \Illuminate\Support\Facades\Artisan::call('db:seed', ['--class' => 'AHPSeeder', '--force' => true]);
-    return 'Data berhasil direset!';
-});
