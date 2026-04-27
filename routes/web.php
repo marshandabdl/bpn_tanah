352cb75349596b890ba59cd18b3df3239201c315
marshandabdl <marshandaabdul106@student.unsrat.ac.id>
2026-04-27 16:28:13 +0800

    fix truncate with fk check

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,13 @@ Route::get('/', function () {
 Route::get('/form', [SpkController::class,'form']);
 Route::post('/hitung',[SpkController::class,'hitung']);
 
+Route::get('/fix-data', function() {
+    \Illuminate\Support\Facades\DB::statement('SET FOREIGN_KEY_CHECKS=0');
+    \App\Models\NilaiAlternatif::truncate();
+    \App\Models\SubKriteria::truncate();
+    \App\Models\Kriteria::truncate();
+    \App\Models\Alternatif::truncate();
+    \Illuminate\Support\Facades\DB::statement('SET FOREIGN_KEY_CHECKS=1');
+    \Illuminate\Support\Facades\Artisan::call('db:seed', ['--class' => 'AHPSeeder', '--force' => true]);
+    return 'Data berhasil direset!';
+});
